ef25993d64f26a483db6b41c50bb69cbafc8a27d
Adam Ingram <everett.ingram22@gmail.com>
2026-06-21 14:48:18 -0400

    Project Created and Learning Objectives listed out.

--- CSharpConsolePlayground/CSharpConsolePlayground/Program.cs
+++ CSharpConsolePlayground/CSharpConsolePlayground/Program.cs
@@ -1,4 +1,28 @@
-﻿namespace CSharpConsolePlayground;
+﻿/*
+ * File:        Program.cs
+ * Project:     CSharpConsolePlayground
+ * Author:      Adam Ingram
+ * Created:     June 2026
+ *
+ * Description:
+ * This is a guided C# learning project built to explore core language concepts
+ * through hands-on console application development. Topics covered include
+ * variables and data types, control flow, methods, classes and OOP principles,
+ * collections, exception handling, file I/O, and LINQ.
+ *
+ * Learning Progression:
+ * 1. Variables & Data Types
+ * 2. Control Flow
+ * 3. Methods
+ * 4. Classes & Objects
+ * 5. Collections
+ * 6. Object-Oriented Principles
+ * 7. Exception Handling
+ * 8. File I/O
+ * 9. LINQ
+ */
+
+namespace CSharpConsolePlayground;
 
 class Program
 {
