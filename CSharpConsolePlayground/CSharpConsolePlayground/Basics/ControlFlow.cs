cbbb8fd9bdd1a118a1caa7a80dd4944993c8f524
Adam Ingram <everett.ingram22@gmail.com>
2026-06-21 16:01:49 -0400

    Control Flow Assignment set up.

---
+++ CSharpConsolePlayground/CSharpConsolePlayground/Basics/ControlFlow.cs
@@ -0,0 +1,40 @@
+/*
+ * File:        ControlFlow.cs
+ * Folder:      Basics
+ * Topic:       Control Flow
+ *
+ * Description:
+ * Demonstrates how C# controls the flow of program execution using conditional
+ * statements and loops. Control flow determines which code runs, when it runs,
+ * and how many times it runs.
+ *
+ * Concepts Covered:
+ * - if / else if / else
+ * - switch statements
+ * - for loop
+ * - while loop
+ * - do while loop
+ * - foreach loop
+ *
+ * Task:
+ * Build a simple grade evaluator and a countdown timer using control flow.
+ * Use each loop type and conditional type at least once.
+ *
+ * Requirements:
+ * - Use if/else if/else to evaluate a numeric score and print a letter grade
+ * - Use a switch statement to print a description based on that grade
+ * - Use a for loop to count up from 1 to 5
+ * - Use a while loop to count down from 5 to 1
+ * - Use a foreach loop to iterate over a list of names and print each one
+ *
+ * Stretch Goal:
+ * Add a do/while loop that keeps asking the user to enter a number greater
+ * than 10 until they actually do. Look up Console.ReadLine() to get user input.
+ */
+
+namespace CSharpConsolePlayground.Basics;
+
+public class ControlFlow
+{
+    
+}
